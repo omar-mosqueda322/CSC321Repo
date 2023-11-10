ede0f120bc8d8fa9e52bd48354a20c75847ecc59
omar-mosqueda322 <omosqueda1@toromail.csudh.edu>
2023-11-10 02:27:59 +0000

    adding my python lab program

---
+++ omosqueda11.py
@@ -0,0 +1,10 @@
+#Omar Mosqueda
+#CSC 321 
+#Lab 11
+#Squaring a number
+
+number = int(input("Enter a number to square: "))
+squaredNumber = pow(number,2)
+print("The number is: ", squaredNumber)
+
+
